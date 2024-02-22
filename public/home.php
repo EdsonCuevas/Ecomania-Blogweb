254994a35805e5a064a81a060a8b2e4c886572df
EdsonCuevas <efelix@ucol.mx>
2024-02-21 23:06:48 -0600

    upload

---
+++ public/home.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Home - Ecomania</title>
+
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+
+      
+
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="assets/css/headers.css" rel="stylesheet">
+  </head>
+  <body>
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <img src="assets/imgs/icon.png" alt="" width="100" height="62" style="padding-left: 30px; object-fit: cover;";">
+        </a>
+      </div>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" class="nav-link px-2">Features</a></li>
+        <li><a href="#" class="nav-link px-2">Pricing</a></li>
+        <li><a href="#" class="nav-link px-2">FAQs</a></li>
+        <li><a href="#" class="nav-link px-2">About</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end" style="padding-right: 30px;">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+      </div>
+    </header>
+ 
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
