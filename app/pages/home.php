7a9c9aade72770e65c29f658856faf5020ed0157
EdsonCuevas <efelix@ucol.mx>
2024-03-01 19:07:40 -0600

    del FAQs in pages

--- app/pages/home.php
+++ app/pages/home.php
@@ -104,7 +104,6 @@
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="<?=ROOT?>/home" class="nav-link px-2 link-secondary">Home</a></li>
         <li><a href="<?=ROOT?>/news" class="nav-link px-2">News</a></li>
-        <li><a href="#" class="nav-link px-2">FAQs</a></li>
         <li><a href="#" class="nav-link px-2">About</a></li>
       </ul>
 
--- app/pages/news.php
+++ app/pages/news.php
@@ -100,7 +100,6 @@
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="<?=ROOT?>/home" class="nav-link px-2">Home</a></li>
         <li><a href="#" class="nav-link px-2 link-secondary">News</a></li>
-        <li><a href="#" class="nav-link px-2">FAQs</a></li>
         <li><a href="#" class="nav-link px-2">About</a></li>
       </ul>
 
