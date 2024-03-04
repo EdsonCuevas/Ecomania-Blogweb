2d2127811422c265ffae183648fb8e727ff853b7
EdsonCuevas <efelix@ucol.mx>
2024-03-03 18:19:50 -0600

    about add

--- app/pages/news.php
+++ app/pages/news.php
@@ -4,10 +4,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.122.0">
-    <title>Album example · Bootstrap v5.3</title>
+    <title>News - Ecomania</title>
 
     <link href="<?=ROOT?>/../public/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
