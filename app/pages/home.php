df001188b2aa696f40ef3943b705a7bd2e3b9f43
EdsonCuevas <efelix@ucol.mx>
2024-03-08 23:52:41 -0600

    nav bar

--- app/pages/home.php
+++ app/pages/home.php
@@ -102,10 +102,9 @@
 
       <ul class="nav nav-pills">
         <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="<?=ROOT?>/news" class="nav-link">News</a></li>
+        <li class="nav-item"><a href="<?=ROOT?>/about" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
       </ul>
     </header>
 
