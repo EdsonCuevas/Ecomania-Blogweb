a0e2ceb194c4827903e4295d56a92c9ad4d8b981
EdsonCuevas <efelix@ucol.mx>
2024-03-11 19:03:32 -0600

    add new header

--- app/pages/home.php
+++ app/pages/home.php
@@ -13,24 +13,23 @@
     <link href="<?=ROOT?>/../public/assets/css/carousel.css" rel="stylesheet">
   </head>
   <body style="padding: 0;">
-  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
+    
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
       <div class="col-md-3 mb-2 mb-md-0">
-        <a href="#" class="d-inline-flex link-body-emphasis text-decoration-none">
-          <img src="<?=ROOT?>/../public/assets/imgs/icon.png" width="40" height="32" role="img" alt="">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <img src="<?=ROOT?>/../public/assets/imgs/icon.png" alt="" width="70" height="67">
         </a>
       </div>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2">Features</a></li>
-        <li><a href="#" class="nav-link px-2">Pricing</a></li>
-        <li><a href="#" class="nav-link px-2">FAQs</a></li>
+        <li><a href="#" class="nav-link px-2">News</a></li>
         <li><a href="#" class="nav-link px-2">About</a></li>
+        <li><a href="#" class="nav-link px-2">Contact</a></li>
       </ul>
 
       <div class="col-md-3 text-end">
         <button type="button" class="btn btn-outline-primary me-2">Login</button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
       </div>
     </header>
 
