771b43f4a29530ac38b63e18a5f9df57fd63e685
EdsonCuevas <efelix@ucol.mx>
2024-03-24 23:53:48 -0600

    base crud data

--- app/pages/admin/users.php
+++ app/pages/admin/users.php
@@ -1,1 +1,33 @@
-users
+<h4>Users</h4>
+
+<table class="table">
+
+    <tr>
+        <th>#</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Image</th>
+        <th>Date</th>
+    </tr>
+
+    <?php
+
+        $query = "select * from users order by id desc";
+        $rows = query($query);
+
+    ?>
+
+    <?php if(!empty($rows)):?>
+        <?php foreach($rows as $row):?>
+        <tr>
+            <td><?=$row['id']?></td>
+            <td><?=$row['username']?></td>
+            <td><?=$row['email']?></td>
+            <td><?=$row['role']?></td>
+            <td>Image</td>
+            <td><?=date("jS M, Y",strtotime($row['date']))?></td>
+        </tr>
+        <?php endforeach;?>
+    <?php endif;?>
+</table>
