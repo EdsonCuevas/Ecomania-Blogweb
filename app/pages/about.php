f276b92728e988ef083f0cab95e8beb40dd33ee5
EdsonCuevas <efelix@ucol.mx>
2024-04-01 22:54:25 -0600

    components page

--- app/pages/about.php
+++ app/pages/about.php
@@ -1,52 +1,6 @@
-<!doctype html>
-<html lang="en" data-bs-theme="auto">
-  <head><script src="../assets/js/color-modes.js"></script>
+<?php include '../app/pages/includes/header.php'; ?>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>About - Ecomania</title>
-
-    <link href="<?=ROOT?>/../public/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    
-  </head>
-  <body style="padding: 0; background-color: #D8F4CC;">
-
-  <header class="p-3 mb-1 border-bottom">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="<?=ROOT?>/home" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
-          <img src="<?=ROOT?>/../public/assets/imgs/icon.png" alt="" width="55" height="50">
-        </a>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="<?=ROOT?>/home" class="nav-link px-2 link-body-emphasis">Home</a></li>
-          <li><a href="<?=ROOT?>/news" class="nav-link px-2 link-body-emphasis">News</a></li>
-          <li><a href="<?=ROOT?>/about" class="nav-link px-2 link-secondary">About</a></li>
-          <li><a href="<?=ROOT?>/contact" class="nav-link px-2 link-body-emphasis">Contact</a></li>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-        </form>
-
-        <div class="dropdown text-end">
-          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-          </a>
-          <ul class="dropdown-menu text-small" style="">
-            <li><a class="dropdown-item" href="<?=ROOT?>/admin">Admin</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="<?=ROOT?>/logout">Sign out</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </header>
-
-
-  <main>
+<main>
   <div class="col-lg-8 mx-auto" style="padding: 10px;">
 
     <div class="row featurette">
@@ -91,29 +45,9 @@
   </div>
       
       
-  </main>
-  <footer class="container">
-  <div class="row">
-        <div class="col-lg-4">
-            <h5>Get to know us</h5>
-            <p><a href="<?=ROOT?>/about">About us</a></p>
-            <p><a href="<?=ROOT?>/news">News</a></p>
-        </div>
-        <div class="col-lg-4">
-            <h5>Terms and Conditions</h5>
-            <p><a href="#">Privacy Policy</a></p>
-            <p>&copy; 2024 Universidad de Colima, Inc. Ecomania</p>
-        </div>
-        <div class="col-lg-4">
-            <h5>Contact us</h5>
-            <p>Ecomania2023@gmail.com</p>
-            <p>Manzanillo, Colima</p>
-        </div>
-    </div>
-    <p class="float-end"><a href="#">Back to top</a></p>
-  </footer>
-</div>
-<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+</main>
 
-    </body>
+<?php include '../app/pages/includes/footer.php'; ?>
+
+</body>
 </html>
