3204e3bf2fcf456b91c1416a288f3762a55316bf
EdsonCuevas <efelix@ucol.mx>
2024-04-03 00:22:52 -0600

    news reworked

--- app/pages/news.php
+++ app/pages/news.php
@@ -2,22 +2,11 @@
 
 <main>
 
-  <section class="py-5 text-center container">
-    <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">News</h1>
-        <p class="lead text-body-secondary">In this section, you will find current news highlights on one of the most pressing challenges facing our planet: climate change. Focusing our attention on Goal 13 of the UN's 2030 Agenda, "Climate Action," we aim to explore how this global phenomenon, accelerated by human activities such as the burning of fossil fuels and deforestation, is significantly impacting Mexico and the world.</p>
-        <p>
-          <a href="#" class="btn btn-success my-2">Main call to action</a>
-          <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-        </p>
-      </div>
-    </div>
-  </section>
-
   <div class="container" style="">
         <div class="row">
 
+          <center><h1>News</h1></center>
+          
           <?php
 
             $limit = 10;
