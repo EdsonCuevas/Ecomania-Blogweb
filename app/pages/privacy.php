2fd4628e8e677dd8068faaad3c59eb738d467af7
EdsonCuevas <efelix@ucol.mx>
2024-04-13 14:43:06 -0600

    add privacy page

---
+++ app/pages/privacy.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Ecomania | Privacy Policy</title>
+    <link rel="stylesheet" href="./assets/css/gridder.css">
+    <link rel="stylesheet" href="./assets/css/app.css">
+
+    <meta name="theme-color " content="#ffffff ">
+</head>
+<body>
+
+<header class="header graphic-4 ">
+	<div class="container ">
+    <div class="navigation ">
+        <div class="row ">
+            <div class="tiny-4 small-4 medium-4 ">
+                <label for="nav" class="nav-open"><i></i><i></i><i></i></label>
+                <a href="<?=ROOT?>/home" class="logo">
+                    <img src="<?=ROOT?>/../public/assets/imgs/icon.png" alt="logo" style="width: 70px;">
+                </a>
+            </div>
+            <?php include '../app/pages/includes/header.php';?>
+        </div>
+    </div>
+</div>
+	<div class="container ">
+		<div class="row ">
+			<div class="tiny-12 tiny-center">
+				<h1>Privacy Policy</h1>
+			</div>
+		</div>
+	</div>
+</header>
+
+<svg style="background:transparent;position:relative;top:-50px;" width="100%" height="50px " viewBox="0 0 1440 130 " preserveAspectRatio="none " xmlns="http://www.w3.org/2000/svg " xmlns:xlink="http://www.w3.org/1999/xlink " style="transform-origin:50%50%;transform:rotateX(180deg);">
+	<path  id="wave-bottom-white " fill="var(--clr-light)" d="M0,56.050526 C121.352261,18.683509 262.172393,0 422.460394,0 C662.892396,0 818.88453,115.474324 1058.94661,115.474324 C1218.988,115.474324 1342.92081,94.808741 1440,56 L1440,130 L0,130 L0,56.050526 Z " ></path>
+</svg>
+
+<main>
+	<section id="articles ">
+		<div class="container ">
+			<div class="row">   
+				
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nesciunt saepe ut suscipit ab sunt ad deserunt, magnam ipsam rerum perspiciatis facilis, quibusdam esse consectetur nostrum quia quo asperiores ratione!</p>
+
+			</div>
+		</div>
+	</section>
+</main>
+
+<!--Footer--->
+
+<?php include '../app/pages/includes/footer.php';?>
+
+<script src="<?=ROOT?>/../public/assets/js/countUp.js"></script>
+<script src="<?=ROOT?>/../public/assets/js/app.js"></script>
+
+</body>
+</html>
+
