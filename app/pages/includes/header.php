80ecb15ca32a6e7a0e5cddd45359144c0abaa332
EdsonCuevas <efelix@ucol.mx>
2024-04-16 00:16:39 -0600

    user icon img resposive

--- app/pages/includes/header.php
+++ app/pages/includes/header.php
@@ -1,6 +1,35 @@
+<style>
+    @media screen and (max-width: 1023px) {
+        .user {
+            position: relative;
+            /* Adjust positioning as needed */
+            top: -380px;
+            /* Adjust top position */
+            left: -310px;
+            /* Remove or adjust left position */
+            right: 0px;
+            /* Consider right-alignment on smaller screens */
+        }
+
+        .user img {
+            width: 50px;
+            /* Adjust width for smaller icons */
+            height: 50px;
+            /* Adjust height for smaller icons */
+        }
+    }
+
+    @media screen and (min-width: 1024px) {
+        .user {
+            top: 10px;
+            left: 10px;
+        }
+    }
+</style>
+
 <div class="tiny-8 small-8 medium-8 tiny-end" style="position:relative;">
     <nav>
-        <input type="checkbox" id="nav" class="hidden"/>
+        <input type="checkbox" id="nav" class="hidden" />
         <label for="nav" class="nav-open"></label>
         <div class="nav-container">
             <ul>
@@ -9,17 +38,19 @@
                 <li><a href="<?=ROOT?>/contact">Contact</a></li>
                 <li><a href="<?=ROOT?>/about">About us</a></li>
                 <?php if(!logged_in()):?>
-                    <a href="<?=ROOT?>/login" style="position: relative; top: 10px; left: 20px;">
-                        <img src="<?=ROOT?>/../public/assets/imgs/user-icon.png" style="" width="36" height="36">
-                    </a>
+                <a href="<?=ROOT?>/login" class="user" style="position: relative;">
+                    <img src="<?=ROOT?>/../public/assets/imgs/user-icon.png" width="36" height="36">
+                </a>
                 <?php endif;?>
 
                 <?php if(logged_in()):?>
-                    <a href="<?=ROOT?>/admin" style="position: relative; top: 15px; left: 20px;">
-                        <img src="<?=get_image(user('image'))?>" style="object-fit: cover; border-radius:20px;" width="40" height="40">
-                    </a>
+                <a href="<?=ROOT?>/admin" class="user">
+                    <img src="<?=get_image(user('image'))?>"
+                        style="object-fit: cover; border-radius:30px; position: relative; top: 15px; left: 10px;"
+                        width="42" height="42">
+                </a>
                 <?php endif;?>
-                
+
             </ul>
         </div>
     </nav>
