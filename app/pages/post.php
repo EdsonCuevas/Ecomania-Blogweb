1a21373905bce6f52b93a9955d0cb35e90b48a15
EdsonCuevas <efelix@ucol.mx>
2024-04-17 07:35:39 -0600

    comments

--- app/pages/post.php
+++ app/pages/post.php
@@ -97,6 +97,23 @@
 			</div>
 		</div>
 	</section>
+
+    <div id="disqus_thread" style="margin: 100px;"></div>
+        <script>
+            var disqus_config = function () {
+                this.page.url = window.location.href;  // Set the page URL to the current browser URL
+                this.page.identifier = '<?= $slug ?>'; // Use PHP to dynamically output the slug
+            };
+
+            (function() {
+                var d = document, s = d.createElement('script');
+                s.src = 'https://EXAMPLE.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
 </main>
 
 <!--Footer--->
@@ -105,6 +122,7 @@
 
 <script src="<?=ROOT?>/../public/assets/js/countUp.js"></script>
 <script src="<?=ROOT?>/../public/assets/js/app.js"></script>
+<script id="dsq-count-scr" src="//your-disqus-shortname.disqus.com/count.js" async></script>
 
 </body>
 </html>
