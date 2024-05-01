f98efed3faa6912368c79134abb442e05c48345f
EdsonCuevas <efelix@ucol.mx>
2024-04-30 21:16:33 -0600

    del trash image and script

--- app/pages/noaccess.php
+++ app/pages/noaccess.php
@@ -11,8 +11,6 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Ecomania | Not Access</title>
     <link rel="shortcut icon" href="<?=ROOT?>/../public/assets/imgs/icon.png">
@@ -24,8 +22,6 @@
     <!-- CSS Files -->
     <link href="<?=ROOT?>/../public/assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="<?=ROOT?>/../public/assets/css/light-bootstrap-dashboard.css?v=2.0.0 " rel="stylesheet" />
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href=".<?=ROOT?>/../public/assets/css/demo.css" rel="stylesheet" />
     
 </head>
 
@@ -236,12 +232,8 @@
 <script src="<?=ROOT?>/../public/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
 <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
 <script src="<?=ROOT?>/../public/assets/js/plugins/bootstrap-switch.js"></script>
-<!--  Google Maps Plugin    -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
 <!--  Chartist Plugin  -->
 <script src="<?=ROOT?>/../public/assets/js/plugins/chartist.min.js"></script>
-<!--  Notifications Plugin    -->
-<script src="<?=ROOT?>/../public/assets/js/plugins/bootstrap-notify.js"></script>
 <!-- Control Center for Light Bootstrap Dashboard: scripts for the example pages etc -->
 <script src="<?=ROOT?>/../public/assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
 <!-- Light Bootstrap Dashboard DEMO methods, don't include it in your project! -->
